cdf63b7c75d9a248309b0995a82e0d01c9bcd285
Hugo Marciano <hmarciano@gmail.com>
2015-02-24 15:17:58 -0300

    Implementação das classes de Login

---
+++ src/main/java/com/upschool/entity/Usuario.java
@@ -0,0 +1,64 @@
+package com.upschool.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USUARIO")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(generator = "USUARIO_SQ", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(allocationSize = 1, initialValue = 1, sequenceName = "USUARIO_SEQ", name = "USUARIO_SQ")
+	private int id;
+
+	@Column(name = "LOGIN", unique = true, nullable = false, updatable = false)
+	private String login;
+
+	@Column(name = "SENHA", nullable = false)
+	private String senha;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_pessoa")
+	private Pessoa pessoa;
+
+	public int getId() {
+		return id;
+	}
+
+	private void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+}
