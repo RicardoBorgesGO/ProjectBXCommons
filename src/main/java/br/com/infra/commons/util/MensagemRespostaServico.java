4bd2233b80f18fb72cbc4a5c3e92cf122e2c1bf8
Hugo Marciano <hmarciano@gmail.com>
2015-03-19 10:31:57 -0300

    Atribuição dos códigos das Mensagens de Resposta. Devem seguir o padrão incremental.

--- src/main/java/br/com/infra/commons/util/MensagemRespostaServico.java
+++ src/main/java/br/com/infra/commons/util/MensagemRespostaServico.java
@@ -2,17 +2,13 @@ package br.com.infra.commons.util;
 
 public enum MensagemRespostaServico {
 	
-	SUCESSO_SALVAR(100, "Sucesso ao salvar."),
-	
-	SUCESSO_ATUALIZADAR(100, "Sucesso ao atualizar."),
-
-	ERRO_NAO_SALVO(200, "Erro ao salvar."),
-	
-	ERRO_NAO_ATUALIZADO(200, "Erro ao atualizar."),
-
 	SUCESSO_LOGIN_REALIZADO(100, "Login realizado com sucesso."),
+	SUCESSO_SALVAR(101, "Sucesso ao salvar."),
+	SUCESSO_ATUALIZADAR(102, "Sucesso ao atualizar."),
 
 	ERRO_USUARIO_NAO_ENCONTRADO(200, "Usuário não encontrado."),
+	ERRO_NAO_SALVO(201, "Erro ao salvar."),
+	ERRO_NAO_ATUALIZADO(202, "Erro ao atualizar."),
 
 	ERRO_NAO_CADASTRADO(999, "Erro não cadastrado.");
 
